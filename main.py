4658588c6e5e3af27d54e00c10508d3cc2e5a158
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-02-14 22:26:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+screen = Screen()
+screen.title("PONG GAME")
+screen.bgcolor("black")
+screen.screensize(canvwidth=800, canvheight=600)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=4, stretch_len=0.5, outline=None)
+paddle.penup()
+
+print("hi world")
