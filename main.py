cef6474afe57a570fc087db19797a71c414369c0
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-02-15 16:51:23 +0000

    Commit 3: enabled movement of paddle so can now move up and down using keystrokes

--- main.py
+++ main.py
@@ -3,14 +3,35 @@ from turtle import *
 screen = Screen()
 screen.title("PONG GAME")
 screen.bgcolor("black")
-screen.screensize(canvwidth=800, canvheight=600)
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
 
 paddle = Turtle()
 paddle.shape("square")
 paddle.color("white")
-paddle.shapesize(stretch_wid=4, stretch_len=0.5, outline=None)
+paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
+paddle.setx(350)
 
-print("Hello world")
-print("Hello world")
-print("hello poeple")
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
