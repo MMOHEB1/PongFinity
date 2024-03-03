2367ac3e00913e0651656c6db7b71c0a43878db2
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-03-03 21:25:27 +0000

    Commit 11: successfully added a proper scoring system that tracks the games score.

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,10 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
         self.goto(100, 200)
@@ -16,8 +20,8 @@ class Scoreboard(Turtle):
 
     def add_l_score(self):
         self.l_score += 1
-        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.update_scoreboard()
 
     def add_r_score(self):
         self.r_score += 1
-        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+        self.update_scoreboard()
